a03a1738c9aa3f5f69db19aa87f4255a3cb680af
Coert Vonk <coert@spare-ws.vonk>
2020-07-29 09:58:31 -0700

    transmits successfully

--- interface/main/rs485/rs485.h
+++ interface/main/rs485/rs485.h
@@ -19,6 +19,7 @@ typedef int (* rs485_read_bytes_fnc_t)(uint8_t * dst, uint32_t len);
 typedef int (* rs485_write_bytes_fnc_t)(uint8_t * src, size_t len);
 typedef int (* rs485_write_fnc_t)(uint8_t src);
 typedef void (* rs485_flush_fnc_t)(void);
+typedef void (* rx485_tx_mode_fnc_t)(bool const tx_enable);
 typedef void (* rs485_queue_fnc_t)(rs485_handle_t const handle, datalink_pkt_t * const pkt);
 typedef datalink_pkt_t * (* rs485_dequeue_fnc_t)(rs485_handle_t const handle);
 
@@ -28,17 +29,13 @@ typedef struct rs485_instance_t {
     rs485_write_bytes_fnc_t write_bytes;
     rs485_write_fnc_t write;
     rs485_flush_fnc_t flush;
+    rx485_tx_mode_fnc_t tx_mode;
     rs485_queue_fnc_t queue;
     rs485_dequeue_fnc_t dequeue;
     QueueHandle_t tx_q;
 } rs485_instance_t;
 
-typedef enum RS485_DIR_t {
-    RS485_DIR_rx = 0,
-    RS485_DIR_tx = 1
-} RS485_DIR_t;
-
-typedef struct rs485_q_msg_t {
+typedef struct  rs485_q_msg_t{
     datalink_pkt_t * pkt;
 } rs485_q_msg_t;
 
