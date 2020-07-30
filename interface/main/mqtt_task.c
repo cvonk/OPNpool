f90327def85557869c101de66b7a469454f8e7ec
Coert Vonk <coert@spare-ws.vonk>
2020-07-30 14:40:22 -0700

    cp

--- interface/main/mqtt_task.c
+++ interface/main/mqtt_task.c
@@ -219,7 +219,6 @@ mqtt_task(void * ipc_void)
                     char const * const topic = msg.data;
                     char * message = strchr(msg.data, '\t');
                     *message++ = '\0';
-                    ESP_LOGI(TAG, "Publish \"%s\": \"%s\"", topic, message);
                     esp_mqtt_client_publish(client, topic, message, strlen(message), 1, 0);
                     free(msg.data);
                     break;
