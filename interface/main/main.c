411405e502965a6830baac69e90d4400243bedb9
cvonk <Coert.Vonk@gmail.com>
2022-04-07 14:26:38 -0700

    update license

--- interface/main/main.c
+++ interface/main/main.c
@@ -4,12 +4,15 @@
  * © Copyright 2014, 2019, 2022, Coert Vonk
  * 
  * This file is part of OPNpool.
+ * 
  * OPNpool is free software: you can redistribute it and/or modify it under the terms of
  * the GNU General Public License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
+ * 
  * OPNpool is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
+ * 
  * You should have received a copy of the GNU General Public License along with OPNpool. 
  * If not, see <https://www.gnu.org/licenses/>.
  * 
