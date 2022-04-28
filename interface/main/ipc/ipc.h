bc3ecd0ce57f405961400802005f3e4617407f72
Coert Vonk <coert.vonk@gmail.com>
2022-04-28 12:53:27 -0700

    fix #define

--- interface/main/ipc/ipc.h
+++ interface/main/ipc/ipc.h
@@ -12,7 +12,7 @@
 #ifndef MIN
 #define MIN(a,b) ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a < _b ? _a : _b; })
 #endif
-#ifndef MIN
+#ifndef MAX
 #define MAX(a,b) ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a > _b ? _a : _b; })
 #endif
 
